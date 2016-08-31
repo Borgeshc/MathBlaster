d2e80973d2676c24e549d87f64c4237f6db17fe9
Borgeshc <hborges@digitaliris.com>
2016-08-31 13:53:27 -0500

    Initial Build

---
+++ Assets/AsteroidMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidMovement : MonoBehaviour
+{
+    public float speed;
+
+	void Update ()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+	}
+}
