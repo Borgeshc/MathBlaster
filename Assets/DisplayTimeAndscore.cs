b53007a7530eeca3e14228764f5975b62b9c77cf
turtenaj92 <turtenaj@gmatc.matc.edu>
2016-09-14 16:07:00 -0500

    made the game over screen
    Hey guys, i made the game over screen. I added a display text script
    which displays the total score and time taken to the canvas.  I also
    added a play again button which goes back to the game and a quit button
    to go back to the main menu.

---
+++ Assets/DisplayTimeAndscore.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DisplayTimeAndscore : MonoBehaviour
+{
+    float totalPoints = 6969;
+    float timeTaken = 30.2f;
+    public Text things;
+	// Update is called once per frame
+	public void OnGUI ()
+    {
+     things.text = ("You scored:" + " " + totalPoints + " points and you took:" + " " + timeTaken + " seconds long to complete");
+	}
+}
