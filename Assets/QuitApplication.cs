76992406da6592f2225225f420ec91fe25cd5d5c
turtenaj92 <turtenaj@gmatc.matc.edu>
2016-09-14 14:30:47 -0500

    Made the Start Menu for the Game
    I made the start menu screen for the game. It contains a start button,
    an instructions menu, an difficulty options menu, and a quit button. I
    added two scripts to the project, just a change scene script that is
    attached to the start game button and the game difficulities button and
    a quit application script for the quit button.

---
+++ Assets/QuitApplication.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitApplication : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	 public void Quit ()
+    {
+        Application.Quit();
+	}
+}
