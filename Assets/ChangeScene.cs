67f29583154655777500461443fd2d683aa0bc7d
Borgeshc <Borgeshc@gmatc.matc.edu>
2016-09-21 12:02:03 -0500

    Merged main menu into dev

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.SceneManagement;
+
+/* * * * * * * * * * * *
+ *                     *
+ *   Self-Explanatory  *
+ *                     *
+ * * * * * * * * * * * */
+
+public class ChangeScene : MonoBehaviour
+{	
+	public void LoadScene (string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
---
+++ Assets/QuitApplication.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* * * * * * * * * * * *
+ *                     *
+ *   Self-Explanatory  *
+ *                     *
+ * * * * * * * * * * * */
+
+public class QuitApplication : MonoBehaviour
+{
+	// Update is called once per frame
+	 public void Quit ()
+    {
+        Application.Quit();
+	}
+}
